b2bc394c8b5eeda82052099e87761c1f02eb39e7
Bhargav <bhargavdash@gmail.com>
2025-07-15 22:25:01 +0530

    Changed title

--- apps/ws-backend/src/redis/client.ts
+++ apps/ws-backend/src/redis/client.ts
@@ -1,9 +1,11 @@
 import { Redis } from 'ioredis'
 
-const redis = new Redis()
 
-const pub = new Redis()
-const sub = new Redis()
+const redis = new Redis("rediss://default:AYZAAAIjcDFkZTFlZDEwNjJjYmE0NzQzOGFiYWU0MzI5MzI2ZjhkNHAxMA@right-ray-34368.upstash.io:6379");
+
+const pub = new Redis("rediss://default:AYZAAAIjcDFkZTFlZDEwNjJjYmE0NzQzOGFiYWU0MzI5MzI2ZjhkNHAxMA@right-ray-34368.upstash.io:6379");
+
+const sub = new Redis("rediss://default:AYZAAAIjcDFkZTFlZDEwNjJjYmE0NzQzOGFiYWU0MzI5MzI2ZjhkNHAxMA@right-ray-34368.upstash.io:6379");
 
 pub.on("connect", () => console.log("Redis Pub client is connected"))
 sub.on("connect", () => console.log("Redis Sub client is connected"))
